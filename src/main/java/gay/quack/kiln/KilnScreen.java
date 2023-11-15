fa8170526daa8a872a4c5da85e87cfa5543e494e
riley <riley.t.housden@gmail.com>
2023-11-15 01:31:27 -0500

    backport new changes to 1.20.1

--- src/main/java/gay/quack/kiln/KilnScreen.java
+++ src/main/java/gay/quack/kiln/KilnScreen.java
@@ -10,12 +10,10 @@ import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class KilnScreen extends AbstractFurnaceScreen<KilnScreenHandler> {
-    private static final Identifier LIT_PROGRESS_TEXTURE = new Identifier("container/furnace/lit_progress");
-    private static final Identifier BURN_PROGRESS_TEXTURE = new Identifier("container/furnace/burn_progress");
     private static final Identifier BACKGROUND = new Identifier("textures/gui/container/furnace.png");
 
     public KilnScreen(KilnScreenHandler handler, PlayerInventory inventory, Text title) {
-        super(handler, new FurnaceRecipeBookScreen(), inventory, title, BACKGROUND, LIT_PROGRESS_TEXTURE, BURN_PROGRESS_TEXTURE);
+        super(handler, new FurnaceRecipeBookScreen(), inventory, title, BACKGROUND);
     }
 
 }
